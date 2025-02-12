c7dbdeab468d7c8ec24086e4b8c3ed05c8fe2ca4
Horeng <good92489@gmail.com>
2025-02-12 17:44:12 +0900

    Add a notebook to assist testing API server

---
+++ kafka_utils/models/__init__.py
@@ -0,0 +1,17 @@
+
+# Constants
+MSG_KEY_TIMESTAMP = 'timestamp'
+MSG_KEY_USER = 'user'
+MSG_KEY_TOKEN = 'token'
+MSG_KEY_RESULT = 'result'
+MSG_KEY_DATA = 'data'
+MSG_KEY_MODEL_NAME = 'model_name'
+MSG_KEY_MODEL_NAME_CAMEL = 'modelName'
+MSG_KEY_PAYLOAD = 'payload'
+MSG_KEY_VALUE = 'value'
+MSG_KEY_ERROR = 'error'
+MSG_KEY_REPORT = 'report'
+
+MSG_TIMESTAMP_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
+
+ENCODING_UTF8 = 'utf-8'
